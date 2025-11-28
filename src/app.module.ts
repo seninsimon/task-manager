33d592da34715bb25b33913c5f0bc003e1f25129
senin c simon <118550960+seninsimon@users.noreply.github.com>
2025-11-28 23:11:38 +0530

    feat : add project resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017/taskScheduler'),
-    UsersModule, AuthModule],
+    UsersModule, AuthModule, ProjectsModule],
   controllers: [],
   providers: [],
 })
